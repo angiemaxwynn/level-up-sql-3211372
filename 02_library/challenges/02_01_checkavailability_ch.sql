11b758d91385bb1ff7cc8e607802a1b03b0d197c
angiemaxwynn <161448984+angiemaxwynn@users.noreply.github.com>
2024-04-24 20:16:14 +0000

    library tasks 1,2,3

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,31 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT COUNT(b.Title)
+FROM Books
+WHERE b.Title = 'Dracula';
+
+--3 Books of Dracula--
+SELECT BookID
+FROM Books
+WHERE Title = 'Dracula';
+
+--The book IDs are 12, 60,73--
+
+SELECT COUNT(b.Title)
+FROM Books AS b JOIN Loans AS l ON b.BookID = l.BookID
+WHERE b.Title = 'Dracula'
+AND l.ReturnedDate IS NULL;
+
+--Subtracting the available books(returned) - not returned
+
+
+SELECT
+(SELECT COUNT(Title)
+FROM Books
+WHERE Title = 'Dracula') -
+(SELECT COUNT(b.Title)
+FROM Books AS b JOIN Loans AS l ON b.BookID = l.BookID
+WHERE b.Title = 'Dracula'
+AND l.ReturnedDate IS NULL)
+AS AvailableCopies;
--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,19 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+SELECT PatronID
+FROM Patrons
+WHERE FirstName = 'Jack' AND LastName = 'Vaan';
+
+--PatronID is 50--
+
+SELECT BookID, Title
+FROM Books
+WHERE BARCODE IN ('2855934983', '4043822646');
+--dorian 11, expectations 93
+
+INSERT INTO LOANS (BookID, PatronID, LoanDate, DueDate)
+VALUES ('11', '50', '2022-08-25', '2022-09-08'),
+('93', '50', '2022-08-25', '2022-09-08');
+
