2e58d5d7dbcabd4ffef044ae1b35ad00005de8ef
angiemaxwynn <161448984+angiemaxwynn@users.noreply.github.com>
2024-04-27 15:04:09 +0000

    finals tasks

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,10 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT l.DueDAte, b.Title, p.FirstName, p.Email
+FROM Loans AS l 
+JOIN Books AS b ON l.BookID = b.BookID
+JOIN Patrons AS p ON l.PatronID = p.PatronID
+WHERE l.DueDate = '2022-07-13'
+AND l.ReturnedDate IS NULL;
--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,11 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT COUNT(l.LoanID) AS LoanCount, p.FirstName, p.LastName
+FROM Patrons AS p JOIN LOANS AS l 
+ON p.PatronID = l.PatronID
+GROUP BY l.PatronID
+ORDER BY LoanCount ASC
+LIMIT 10;
+
+
